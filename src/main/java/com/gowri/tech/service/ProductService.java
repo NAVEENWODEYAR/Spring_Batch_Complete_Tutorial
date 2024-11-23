3536f7d3bfee2917d428aaa5cd0b5e96cc2d65c7
Naveen <naveenkrishnawodeyar@gmail.com>
2024-11-23 22:56:31 +0530

    added Product service

---
+++ src/main/java/com/gowri/tech/service/ProductService.java
@@ -0,0 +1,25 @@
+/**
+ * @author -NaveenWodeyar
+ * @date -23-Nov-2024
+ * @time - 10:47:56 pm
+ * @location -
+ */
+package com.gowri.tech.service;
+
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.gowri.tech.entity.EmployeesTable;
+
+/**
+ * 
+ */
+@Service
+public class ProductService {
+	
+private Gson gson;
+public void saveProducts() {
+	
+}
+
+}
