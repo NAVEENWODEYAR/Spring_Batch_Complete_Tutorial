718460ee2aeaab6bf03b850b08684c17fe15af79
Naveen <naveenkrishnawodeyar@gmail.com>
2024-11-24 00:27:05 +0530

    Gson library for Serilization

---
+++ src/main/java/com/gowri/tech/service/Impl/ProjectServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gowri.tech.service.Impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.gowri.tech.entity.Projects;
+import com.gowri.tech.service.ProjectService;
+
+/**
+ * @author -NaveenWodeyar
+ * @date -24-Nov-2024
+ * @time - 12:25:04 am
+ * @location -
+ */
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+	@Override
+	public void saveProducts() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Projects> getProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Projects getProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/gowri/tech/service/ProjectService.java
@@ -0,0 +1,22 @@
+package com.gowri.tech.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.gowri.tech.entity.Projects;
+
+/**
+ * @author -NaveenWodeyar
+ * @date -23-Nov-2024
+ * @time - 10:47:56 pm
+ * @location -
+ */
+@Service
+public interface ProjectService {
+
+	public void saveProducts();
+	public List<Projects> getProducts();
+	public Projects getProduct();
+
+}
